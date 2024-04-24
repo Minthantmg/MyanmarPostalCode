7bec07adf615f75222973c2b6c08b391e47b5624
Minthantmg <minthant180@gmail.com>
2024-04-24 13:06:38 +0900

    added constant

---
+++ Constants/index.ts
@@ -0,0 +1,18 @@
+export const cities = [
+  {
+    id: 1,
+    name: "Yangon",
+  },
+  {
+    id: 2,
+    name: "Mandalay",
+  },
+  {
+    id: 3,
+    name: "Ayarwaddy",
+  },
+  {
+    id: 4,
+    name: "NayPyiTaw",
+  },
+];
