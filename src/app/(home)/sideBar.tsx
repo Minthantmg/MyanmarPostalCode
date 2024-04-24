bf160e28320fb386fddbbbf548607eb42d21b5d0
Minthantmg <minthant180@gmail.com>
2024-04-24 13:07:38 +0900

    added sideBar ui

---
+++ src/app/(home)/sideBar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { cities } from "../../../Constants";
+
+const sideBar = () => {
+  return (
+    <div className="border-r-2 min-h-screen w-1/3">
+      {cities.map((city) => (
+        <div key={city.id} className="border-b-2 p-2 cursor-pointer">
+          {city.name}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default sideBar;
