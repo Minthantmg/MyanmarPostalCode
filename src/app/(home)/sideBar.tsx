4c098bc36e9998354fdac36769e155d82123a40a
Minthantmg <minthant180@gmail.com>
2024-04-25 13:20:46 +0900

    update ui

--- src/app/(home)/sideBar.tsx
+++ src/app/(home)/sideBar.tsx
@@ -3,7 +3,7 @@ import { cities } from "../../../Constants";
 
 const sideBar = () => {
   return (
-    <div className="border-r-2 min-h-screen w-1/3">
+    <div className="border-r-2 min-h-screen w-1/3 sm:w-full">
       {cities.map((city) => (
         <div key={city.id} className="border-b-2 p-2 cursor-pointer">
           {city.name}
