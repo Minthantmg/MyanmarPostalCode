e23ab5e1618b1a4a181b1f518d9a2af310fc9470
Min Thant Mg <48246915+Minthantmg@users.noreply.github.com>
2024-04-25 22:22:25 +0630

    update columns

--- src/app/state/columns.tsx
+++ src/app/state/columns.tsx
@@ -1,36 +1,60 @@
 "use client";
 
-import { ColumnDef } from "@tanstack/react-table";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import {ColumnDef} from "@tanstack/react-table";
+import {ArrowUpDown} from "lucide-react";
+import {Button} from "@/components/ui/button";
 
-export type Postal = {
-  id: string;
-  town_township: string;
-  qv_tract: string;
-  postal_code: string;
-};
+export interface Postal {
+    id: string;
+    "en.town_township": string;
+    "en.qv_tract" : string;
+    postal_code: string;
+}
+
 
 export const columns: ColumnDef<Postal>[] = [
-  {
-    accessorKey: "qv_tract",
-    header: "Quarters",
-    cell: ({ row }) => {
-      const postal_data = row.original;
-      // @ts-ignore
-      return <div>{postal_data.en?.qv_tract}</div>;
+    {
+        accessorKey: "en.qv_tract",
+        header: ({column}) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Quarters
+                    <ArrowUpDown className="ml-2 h-4 w-4"/>
+                </Button>
+
+            )
+        },
+        cell: ({row}) => {
+            const postal_data = row.original;
+            // @ts-ignore
+            return <div>{postal_data.en?.qv_tract}</div>;
+        },
     },
-  },
-  {
-    accessorKey: "town_township",
-    header: "Township",
-    cell: ({ row }) => {
-      const postal_data = row.original;
-      // @ts-ignore
-      return <div>{postal_data.en?.town_township}</div>;
+    {
+        accessorKey: "en.town_township",
+        header: ({column}) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Township
+                    <ArrowUpDown className="ml-2 h-4 w-4"/>
+                </Button>
+
+            )
+        },
+        cell: ({row}) => {
+            const postal_data = row.original;
+            // @ts-ignore
+            return <div>{postal_data.en?.town_township}</div>;
+        },
     },
-  },
-  {
-    accessorKey: "postal_code",
-    header: "PostalCode",
-  },
+    {
+        accessorKey: "postal_code",
+        header: "Township"
+    },
 ];
