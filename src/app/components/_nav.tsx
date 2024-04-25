d94b6278401cec8e30a31f515362814b465b3e4e
Minthantmg <minthant180@gmail.com>
2024-04-25 13:20:56 +0900

    update ui

--- src/app/components/_nav.tsx
+++ src/app/components/_nav.tsx
@@ -4,11 +4,11 @@ import { displayProps } from "../types";
 
 const _nav = ({ toggleDisplay }: displayProps) => {
   return (
-    <div className="flex justify-start items-center border-b-2 p-4 sm:p-8">
+    <div className="flex justify-start items-center border-b-2 p-4">
       <div className="cursor-pointer sm:hidden z-10" onClick={toggleDisplay}>
         <Menu />
       </div>
-      <div className="w-full fixed">
+      <div className="w-full">
         <div className="flex justify-center items-center">
           <div>
             M<span className="text-[10px]">yanmar</span> P
