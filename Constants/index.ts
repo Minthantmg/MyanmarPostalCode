5232f00501844f92434f0a9ecabb388a0d08964d
Minthantmg <minthant180@gmail.com>
2024-04-25 17:44:48 +0900

    update data

--- Constants/index.ts
+++ Constants/index.ts
@@ -2,19 +2,59 @@ import { Postal } from "@/app/(home)/columns";
 
 export const cities = [
   {
-    id: 1,
-    name: "Yangon",
+    id: "01",
+    name: "Kachin",
   },
   {
-    id: 2,
+    id: "02",
+    name: "Sagaing",
+  },
+  {
+    id: "03",
+    name: "Chin",
+  },
+  {
+    id: "04",
+    name: "Magway",
+  },
+  {
+    id: "05",
     name: "Mandalay",
   },
   {
-    id: 3,
-    name: "Ayarwaddy",
+    id: "06",
+    name: "Shan",
   },
   {
-    id: 4,
-    name: "NayPyiTaw",
+    id: "07",
+    name: "Rakhine",
+  },
+  {
+    id: "08",
+    name: "Bago",
+  },
+  {
+    id: "09",
+    name: "Kayah",
+  },
+  {
+    id: "10",
+    name: "Ayeyarwady",
+  },
+  {
+    id: "11",
+    name: "Yangon",
+  },
+  {
+    id: "12",
+    name: "Mon",
+  },
+  {
+    id: "13",
+    name: "Kayin",
+  },
+  {
+    id: "14",
+    name: "Tanintharyi",
   },
 ];
