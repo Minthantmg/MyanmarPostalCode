6b11e24b525f890ef41ca041d826975cfc60a166
Minthantmg <minthant180@gmail.com>
2024-04-26 18:39:24 +0900

    update ui

--- src/app/service-page/page.tsx
+++ src/app/service-page/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const page = () => {
-  return <div>service</div>;
+  return (
+      <div className="w-full h-screen">
+        <div className="flex justify-center items-center">
+          <div className="pt-32">
+            service
+          </div>
+        </div>
+      </div>
+  );
 };
 
 export default page;
