ea2868897a9b2238a7170f5cbf524532784f7456
Minthantmg <minthant180@gmail.com>
2024-04-29 18:51:18 +0900

    update ui

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -2,7 +2,8 @@
 import React, {useEffect} from "react";
 import Image from "next/image";
 import logo from "../../../public/myanmar.png"
-import {ChevronRight} from 'lucide-react';
+import country from "../../../public/background.jpg"
+import {FileJson, Book} from 'lucide-react';
 import {useRouter} from "next/navigation";
 
 const page = () => {
@@ -10,39 +11,84 @@ const page = () => {
     const router = useRouter()
 
     const gotoService = () => {
-        router.push('/service-page')
+        router.push('/docs')
+    }
+
+    const gotoState = () => {
+        router.push('/demo')
     }
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-            router.prefetch('/service-page')
+            router.prefetch('/docs')
+            router.prefetch('/demo')
         }, [router]
     )
     return (
-        <div className="w-full h-screen bg-cover bg-no-repeat bg-center"
-             style={{backgroundImage: `url(/background.jpg)`}}>
-            <div className="flex flex-col justify-center items-center gap-y-16 sm:gap-y-10">
-                <div className="sm:flex pt-24 sm:pt-64 justify-center items-center">
-                    <Image src={logo} alt="" className="sm:w-28 sm:h-28 w-36 h-36 ml-24 sm:ml-0"/>
-                    <div className="text-xl sm:text-3xl text-white font-bold pt-4 sm:pt-0">Postal Codes/ Zip Codes Of
-                        Myanmar
+        <div className="w-full">
+            <div className="sm:flex justify-center items-center sm:pt-24 pt-16 text-justify sm:px-44 sm:pb-[100px] pb-12">
+                <div className="sm:w-1/2 p-6">
+                    <div>
+                        <div className="text-2xl font-bold font-poppin">
+                            Myanmar Postal Code API
+                        </div>
+                        <div className="text-gray-400 pt-10 font-poppin">
+                            Welcome to the Myanmar Postal Code API! Our API provides free access to accurate postal code
+                            data
+                            for locations across Myanmar. Whether you are building a website, mobile app, or integrating
+                            postal
+                            code functionality into your software, our API makes it easy to retrieve postal code
+                            information
+                            quickly and reliably.
+                            <ul className="sm:pt-8 grid gap-y-2 font-poppin">
+                                <li><span className="text-black font-bold">1.Comprehensive Data:</span> Access
+                                    up-to-date postal code information for all regions in
+                                    Myanmar.
+                                </li>
+                                <li><span className="text-black font-bold">2.Easy Integration:</span> Simple API
+                                    endpoints for easy
+                                    integration into your
+                                    applications.
+                                </li>
+                                <li><span className="text-black font-bold">3.Accurate Results:</span> Reliable data
+                                    sourced from
+                                    official postal authorities to ensure
+                                    accuracy.
+                                </li>
+                                <li><span className="text-black font-bold">4.Free to Use:</span> Our API is completely
+                                    free to use
+                                    for both commercial and
+                                    non-commercial
+                                    projects.
+                                </li>
+                            </ul>
+                            <div className="sm:pt-2">
+                                This API is free and grants you unlimited access to our postal code data for your
+                                development needs.
+                            </div>
+                        </div>
                     </div>
-                    <Image src={logo} alt="" className="w-28 h-28 hidden sm:block"/>
                 </div>
-                <div className="text-white text-center text-lg px-4 sm:px-96">
-                    Discover postal codes from myanmar with our easy-to-use search tool. Stay informed about the
-                    myanmar postal system and connect with us for updates.
+                <div className="sm:w-1/2 font-poppin">
+                    <div className="p-4 mt-4 hidden sm:block">
+                        <Image src={country} alt="logo" className="rounded-lg"/>
+                    </div>
+                    <div className="sm:flex justify-center items-center sm:pt-10 sm:gap-14 grid gap-y-4">
+                        <button className="flex justify-between bg-green-400 px-6 py-3 rounded-sm text-white"
+                                onClick={gotoService}>
+                            <span className="pr-4">Read Docs</span>
+                            <Book/>
+                        </button>
+                        <button className="flex justify-between bg-yellow-400 px-6 py-3 rounded-sm text-white"
+                                onClick={gotoState}>
+                            <span className="pr-4">View Demo</span>
+                            <FileJson/>
+                        </button>
+                    </div>
                 </div>
-                <div className="bg-orange-400 rounded-lg" onClick={gotoService}>
-                    <button className="text-white flex justify-between">
-                        <span className="pl-4 pr-2 py-4">Get Start For Use</span>
-                        <span className="px-2 py-4"><ChevronRight/></span>
-                    </button>
-                </div>
-                <div
-                    className="absolute inset-x-0 bottom-0 w-full h-1/3 bg-gradient-to-b from-transparent to-gray-900 opacity-75"></div>
             </div>
-        </div>);
+        </div>
+    );
 };
 
 export default page;
