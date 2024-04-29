8b971d3db591080e5171b45eb8aba20f3f99ef6e
Minthantmg <minthant180@gmail.com>
2024-04-29 18:49:20 +0900

    changed base url

--- utils/axiosInstance.ts
+++ utils/axiosInstance.ts
@@ -1,4 +1,4 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "https://myanmar-posital-code-api.vercel.app/api/data/myanmar",
+  baseURL: "https://myanmar-postal-code.vercel.app/api/data/myanmar",
 });
