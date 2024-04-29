8b2b694646ddf29823df8ed5633f31cfd4be67d6
Minthantmg <minthant180@gmail.com>
2024-04-29 18:49:48 +0900

    added new library

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily:{
+        poppin: ["Poppins"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -74,7 +77,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),require('daisyui')],
 } satisfies Config
 
 export default config
