6e34b62250b0d98b244de38fe2782d91ae78d8a3
Minthantmg <minthant180@gmail.com>
2024-04-30 17:44:17 +0900

    update ui

--- src/app/demo/columns.tsx
+++ src/app/demo/columns.tsx
@@ -4,41 +4,26 @@ import {ColumnDef} from "@tanstack/react-table";
 
 export interface Postal {
     id: string;
-    en: object
-    town_township: string;
-    qv_tract : string;
+    en_region : string;
+    en_town_township : string;
+    en_qv_tract : string;
     postal_code: string;
-    tsp_code : string
 }
 
 
 
 export const columns: ColumnDef<Postal>[] = [
     {
-        accessorKey: "qv_tract",
+        accessorKey: "en_qv_tract",
         header : "Quarters",
-        cell: ({row}) => {
-            const postal_data = row.original;
-            // @ts-ignore
-            return <div>{postal_data.en?.qv_tract}</div>;
-        },
     },
     {
-        accessorKey: "town_township",
+        accessorKey: "en_town_township",
         header: "Township",
-        cell: ({row}) => {
-            const postal_data = row.original;
-            // @ts-ignore
-            return <div>{postal_data.en?.town_township}</div>;
-        },
     },
     {
         accessorKey: "postal_code",
         header: "PostalCode"
     },
 
-    {
-        accessorKey: "tsp_code",
-        header: "TownCode"
-    },
 ];
