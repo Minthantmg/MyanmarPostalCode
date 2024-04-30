481412b1c9055676aa28efd45cfd7907784b5ae2
Minthantmg <minthant180@gmail.com>
2024-04-30 17:42:32 +0900

    added data

--- Constants/index.ts
+++ Constants/index.ts
@@ -1,60 +1,97 @@
-import { Postal } from "@/app/demo/columns";
-
-export const cities = [
+export const data = [
   {
-    id: "01",
-    name: "Kachin",
-  },
-  {
-    id: "02",
-    name: "Sagaing",
-  },
-  {
-    id: "03",
-    name: "Chin",
-  },
-  {
-    id: "04",
-    name: "Magway",
-  },
-  {
-    id: "05",
-    name: "Mandalay",
-  },
-  {
-    id: "06",
-    name: "Shan",
-  },
-  {
-    id: "07",
-    name: "Rakhine",
-  },
-  {
-    id: "08",
-    name: "Bago",
-  },
-  {
-    id: "09",
-    name: "Kayah",
-  },
-  {
-    id: "10",
-    name: "Ayeyarwady",
-  },
-  {
-    id: "11",
-    name: "Yangon",
-  },
-  {
-    id: "12",
-    name: "Mon",
-  },
-  {
-    id: "13",
-    name: "Kayin",
-  },
-  {
-    id: "14",
-    name: "Tanintharyi",
+    "mm_region": "'...',",
+    "mm_town_township": "'...',",
+    "mm_qv_tract": "'...',",
+    "en_region": "'...',",
+    "en_town_township": "'...',",
+    "en_qv_tract": "'...',",
+    "_id": "'...',",
+    "tsp_code": "'...',",
+    "region_code": "'...',",
+    "postal_code": "'...',",
+    "qv_code": "'...',"
   },
 ];
+
+export const dataDetail = [
+  {
+    tsp_code: "01",
+    region: "Kachin State"
+  },
+  {
+    tsp_code: "02",
+    region: "Sagaing Region"
+  },
+  {
+    tsp_code: "03",
+    region: "Chin State"
+  },
+  {
+    tsp_code: "04",
+    region: "Magway Region"
+  },
+  {
+    tsp_code: "05",
+    region: "Mandalay Region"
+  },
+  {
+    tsp_code: "06",
+    region: "Shan State (South)"
+  },
+  {
+    tsp_code: "07",
+    region: "Rakhine State"
+  },
+  {
+    tsp_code: "08",
+    region: "Bago Region (East)"
+  },
+  {
+    tsp_code: "09",
+    region: "Kayah state"
+  },
+  {
+    tsp_code: "10",
+    region: "Ayeyarwady Region"
+  },
+  {
+    tsp_code: "11",
+    region: "Yangon Region"
+  },
+  {
+    tsp_code: "12",
+    region: "Mon State"
+  },
+  {
+    tsp_code: "13",
+    region: "Kayin State"
+  },
+  {
+    tsp_code: "14",
+    region: "Tanintharyi Region"
+  },
+  {
+    tsp_code: "15",
+    region: "Naypyitaw Union Territory"
+  },
+  {
+    tsp_code: "16",
+    region: "Shan State (North)"
+  },
+  {
+    tsp_code: "17",
+    region: "Shan State (East)"
+  },
+  {
+    tsp_code: "18",
+    region: "Bago Region (West)"
+  },
+]
+
+export const regionData = [
+  {
+    region_code: "'... ',",
+    region: "'... ',",
+  },
+]
