35daf8b0bd50832ccffe4207d905370638587ac9
Minthantmg <minthant180@gmail.com>
2024-04-30 17:42:57 +0900

    update ui

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,77 +1,10 @@
 'use client'
-import React, {useState} from "react";
-import {Box, Copy} from "lucide-react";
-import Check from "../../../public/check.png"
-import Image from "next/image";
-import * as url from "url";
+import React from "react";
+import {Box} from "lucide-react";
+import Endpoint from "../../app/components/_endpoint"
+import Usage from "../../app/components/_usage"
 
 const page = () => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [copied, setCopied] = useState(false);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [copiedAll, setCopiedAll] = useState(false);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [outputAll, setOutputAll] = useState(false)
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [output, setOutput] = useState(false)
-
-    const singleText = 'https://myanmar-postal-code.vercel.app/api/data/myanmar?region=01';
-    const allText =  "https://myanmar-postal-code.vercel.app/api/data/myanmar/regions"
-
-    const handleCopy = async () => {
-        try {
-            await navigator.clipboard.writeText(singleText);
-            setCopied(true);
-        } catch (error) {
-        } finally {
-            setTimeout(() => setCopied(false), 5000);
-        }
-    };
-
-    const handleCopyAll = async () => {
-        try {
-            await navigator.clipboard.writeText(allText);
-            setCopiedAll(true);
-        } catch (error) {
-        } finally {
-            setTimeout(() => setCopiedAll(false), 5000);
-        }
-    };
-
-    const handleOutputAll = () => {
-        setOutputAll(!outputAll)
-    }
-
-    const handleOutput = () => {
-        setOutput(!output)
-    }
-
-    const data = [
-        {
-            "en": {
-                "region": "'...',",
-                "town_township": "'...',",
-                "qv_tract": "'...',"
-            },
-            "mm": {
-                "region": "'...',",
-                "town_township": "'...',",
-                "qv_tract": "'...',"
-            },
-            "_id": "'...',",
-            "tsp_code": "'...',",
-            "region_code": "'...',",
-            "postal_code": "'...',",
-            "qv_code": "'...',"
-        },
-    ];
-
-    const dataAll = [
-        [
-            "15",
-            "Naypyitaw Union Territory"
-        ],
-    ]
 
     return (
         <div className="w-full font-poppin">
@@ -92,114 +25,8 @@ const page = () => {
                         States and Postal Codes
                     </div>
                 </div>
-                <div className="text-black text-lg leading-tight">
-                    Get all regions
-                </div>
-                <div className="sm:w-4/5 w-[365px]">
-                    <div className="flex bg-gray-800 text-gray-400 px-4 py-2 rounded-sm">
-                        <div className="w-2/3 text-justify">
-                            {allText}
-                        </div>
-                        <div className="w-1/3 flex justify-end items-center">
-                            {copiedAll ? (
-                                <>
-                                    <Image src={Check} alt="" className="w-6 h-6 ml-4 sm:ml-0 mr-1 sm:mr-0"/>
-                                </>
-                            ) : (
-                                <div onClick={handleCopy}
-                                     className="cursor-pointer ml-4 sm:ml-0 mr-1 sm:mr-0 w-6 h-6">
-                                    <Copy/>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
-                <div onClick={handleOutputAll}>
-                    <button className="border border-green-300 text-black hover:bg-green-300 hover:text-white
-                    px-4 py-2 rounded-sm">Output
-                    </button>
-                </div>
-                {outputAll && (
-                    <>
-                        <div className="flex flex-col">
-                            <span>[</span>
-                            <span>{`{`}
-                                <span>
-                          {data.map((item) => (
-                              <div key={item.postal_code}>
-                                  <h2 className="flex flex-col">en :
-                                      <span className="flex flex-col">{`{`}</span>
-                                      <span>region :{item.en.region}</span>
-                                      <span>town_township :{item.en.town_township}</span>
-                                      <span>qv_tract :{item.en.qv_tract}</span>
-                                      {`}`}
-                                  </h2>
-                                  <h2 className="flex flex-col">mm :
-                                      <span className="flex flex-col">{`{`}</span>
-                                      <span>region :{item.en.region}</span>
-                                      <span>town_township :{item.en.town_township}</span>
-                                      <span>qv_tract :{item.en.qv_tract}</span>
-                                      {`}`}
-                                  </h2>
-                                  <p>_id : {item._id}</p>
-                                  <p>tsp_code : {item.tsp_code}</p>
-                                  <p>region_code : {item.region_code}</p>
-                                  <p>postal_code : {item.postal_code}</p>
-                                  <p>qv_code : {item.qv_code}</p>
-                              </div>
-                          ))}
-                        </span>
-                                {`}`} </span>
-                            <span>]</span>
-                        </div>
-                    </>
-                )}
-                <div className="text-black text-lg leading-tight">
-                    Get a single region
-                </div>
-                <div className="sm:w-4/5 w-[365px]">
-                    <div className="flex bg-gray-800 text-gray-400 px-4 py-2 rounded-sm">
-                        <div className="w-2/3 text-justify">
-                            {singleText}
-                        </div>
-                        <div className="w-1/3 flex justify-end items-center">
-                            {copied ? (
-                                <>
-                                    <Image src={Check} alt="" className="w-6 h-6 ml-4 sm:ml-0 mr-1 sm:mr-0"/>
-                                </>
-                            ) : (
-                                <div onClick={handleCopy}
-                                     className="cursor-pointer ml-4 sm:ml-0 mr-1 sm:mr-0 w-6 h-6">
-                                    <Copy/>
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
-                <div onClick={handleOutput}>
-                    <button className="border border-green-300 text-black hover:bg-green-300 hover:text-white
-                    px-4 py-2 rounded-sm">Output
-                    </button>
-                </div>
-                <div className="pb-14">
-                    {output && (
-                        <>
-                            <div className="flex flex-col">
-                                <span>[</span>
-                                <span>[</span>
-                                <span>
-                            {dataAll.map((item) => (
-                                <div key="1" className="flex flex-col">
-                                    {item}
-                                </div>
-                            ))}
-                            </span>
-                                <span> ]</span>
-                                <span>]</span>
-                            </div>
-                        </>
-                    )}
-                </div>
+                <Endpoint />
+                <Usage />
             </div>
         </div>
     );
