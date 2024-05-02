90d043b5144be384313d0b082e59aeafd9a817ef
Minthantmg <48246915+Minthantmg@users.noreply.github.com>
2024-05-02 18:45:02 +0900

    update ui

--- src/app/home-page/page.tsx
+++ src/app/home-page/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, {useEffect} from "react";
 import Image from "next/image";
-import country from "../../../public/background.jpg"
+import country from "../../../public/myanmar_land.png"
 import {FileJson, Book} from 'lucide-react';
 import {useRouter} from "next/navigation";
 
@@ -25,7 +25,8 @@ const page = () => {
     )
     return (
         <div className="w-full">
-            <div className="sm:flex justify-center items-center sm:pt-24 pt-16 text-justify sm:px-44 sm:pb-[100px] pb-12">
+            <div
+                className="sm:flex justify-center items-center sm:pt-24 pt-16 text-justify sm:px-44 sm:pb-[100px] pb-12">
                 <div className="sm:w-1/2 p-6">
                     <div>
                         <div className="text-2xl font-bold font-poppin">
@@ -65,24 +66,24 @@ const page = () => {
                                 This API is free and grants you unlimited access to our postal code data for your
                                 development needs.
                             </div>
+                            <div className="sm:flex justify-center items-center pt-10 sm:gap-14 grid gap-y-4">
+                                <button className="flex justify-between bg-green-400 px-6 py-3 rounded-sm text-white"
+                                        onClick={gotoService}>
+                                    <span className="pr-4">Read Docs</span>
+                                    <Book/>
+                                </button>
+                                <button className="flex justify-between bg-yellow-400 px-12 py-3 rounded-sm text-white"
+                                        onClick={gotoState}>
+                                    <span className="pr-4">Demo</span>
+                                    <FileJson/>
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="sm:w-1/2 font-poppin">
-                    <div className="p-4 mt-4 hidden sm:block">
+                    <div className="p-4 mt-4 hidden sm:flex justify-center items-center">
                         <Image src={country} alt="logo" className="rounded-lg"/>
-                    </div>
-                    <div className="sm:flex justify-center items-center sm:pt-10 sm:gap-14 grid gap-y-4">
-                        <button className="flex justify-between bg-green-400 px-6 py-3 rounded-sm text-white"
-                                onClick={gotoService}>
-                            <span className="pr-4">Read Docs</span>
-                            <Book/>
-                        </button>
-                        <button className="flex justify-between bg-yellow-400 px-12 py-3 rounded-sm text-white"
-                                onClick={gotoState}>
-                            <span className="pr-4">Demo</span>
-                            <FileJson/>
-                        </button>
                     </div>
                 </div>
             </div>
