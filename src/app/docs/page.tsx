71a381d3a948875e3b93b3ecfb82bca192583da0
Minthantmg <48246915+Minthantmg@users.noreply.github.com>
2024-05-03 16:27:42 +0900

    update ui

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import {Box} from "lucide-react";
 import Endpoint from "../../app/components/_endpoint"
 import Usage from "../../app/components/_usage"
+import detail from "../../../public/detail.png"
+import Image from "next/image";
 
 const page = () => {
 
@@ -17,6 +19,14 @@ const page = () => {
                         Myanmar Postal Code API can be used with any type of project that needs postal code and township
                         of myanmar in JSON format. you can use examples below to check how Myanmar Postal Code API works
                         and feel free to enjoy it in your awesome projects!
+                    </div>
+                </div>
+                <div>
+                    <div className="font-bold text-2xl text-yellow-600">
+                        How to postal code work?
+                    </div>
+                    <div className="pt-6">
+                        <Image src={detail} alt="detail_ing" />
                     </div>
                 </div>
                 <div className="font-bold text-2xl flex items-center gap-2">
